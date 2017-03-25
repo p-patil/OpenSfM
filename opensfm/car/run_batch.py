68ff8a3aca164c83fe6cffb8a31da3794dabf340
Yang Gao <gy20073@gmail.com>
2017-03-24 20:18:19 -0700

    run_batch support orb and dso

--- opensfm/car/run_batch.py
+++ opensfm/car/run_batch.py
@@ -2,15 +2,19 @@ import os
 import subprocess
 import sys
 
-def filter_dirs(dirs):
+def filter_dirs(dirs, program):
     ans = []
     for d in dirs:
         if "_" in d:
             continue
         if not os.path.isdir(d):
             continue
-        if os.path.exists(os.path.join(d, "reconstruction.meshed.json")):
-            continue
+        if program == "opensfm":
+            if os.path.exists(os.path.join(d, "reconstruction.meshed.json")):
+                continue
+        elif program == "dso" or program == "orb":
+            if os.path.exists(os.path.join(d, program+".txt")):
+                continue
         ans.append(d)
     return ans
 
@@ -18,10 +22,8 @@ if __name__ == "__main__":
     base_path = "/data/yang/data/opensfm"
     modn = int(sys.argv[1])
     this = int(sys.argv[2])
-    if len(sys.argv) >=4 and sys.argv[3].lower() == "run_seg":
-        run_seg = True
-    else:
-        run_seg=False
+
+    program = sys.argv[3].lower()
 
     gpus = ["6", "7"]
 
@@ -31,14 +33,41 @@ if __name__ == "__main__":
         dirs.append(full)
 
     dirs = sorted(dirs)
-    dirs = filter_dirs(dirs)
+    dirs = filter_dirs(dirs, program)
 
     for i, dataset in enumerate(dirs):
         if i%modn == this:
             print dataset
-            if run_seg:
-                print "running segmentation only"
+
+            if program == "seg":
                 e = subprocess.call(["bin/run_seg.sh", dataset, gpus[this % len(gpus)]])
-            else:
+            elif program == "opensfm":
                 e=subprocess.call(["bin/run_all", dataset, gpus[this%len(gpus)]])
+            elif program == "dso":
+                relative = "dso/"
+                e=subprocess.call([ relative+"build/bin/dso_dataset",
+                                    "files="+dataset+"/images",
+                                    "calib="+relative+"camera_nexar.txt",
+                                    "preset=2",
+                                    "mode=1",
+                                    "nogui=1",
+                                    "nolog=1"])
+                if not os.path.exists("result.txt"):
+                    subprocess.call(["echo 'error' > result.txt "], shell=True)
+                os.rename("result.txt", os.path.join(dataset, "dso.txt"))
+            elif program == "orb":
+                relative = "ORB_SLAM2/"
+
+                e = subprocess.call([relative + "Examples/Monocular/mono_nexar",
+                                     relative + "Vocabulary/ORBvoc.txt",
+                                     relative + "Examples/Monocular/nexar.yaml",
+                                     dataset + "/images",
+                                     "0"])
+                outname = "KeyFrameTrajectory.txt"
+                if not os.path.exists(outname):
+                    subprocess.call(["echo 'error' > " + outname], shell=True)
+                os.rename(outname, os.path.join(dataset, "orb.txt"))
+            else:
+                raise ValueError("invalid program parameter")
+
             print "error code", e
